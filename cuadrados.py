b303decd76f184cd5cf73c1532b500412e157798
AxelSantiago17 <axel.santiago1531@gmail.com>
2025-04-07 20:10:27 -0600

    Update cuadrados.py

--- cuadrados.py
+++ cuadrados.py
@@ -4,3 +4,9 @@ class Cuadrado:
 
     def calcular_area(self):
         return self.lado ** 2
+
+if __name__ == "__main__":
+    lado = float(input("Ingresa el valor del lado del cuadrado: "))
+    mi_cuadrado = Cuadrado(lado)
+    area = mi_cuadrado.calcular_area()
+    print(f"El área del cuadrado con lado {lado} es: {area}")
